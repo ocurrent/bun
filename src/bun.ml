8a6bb04f4c0daf9353185a4db6d49c7f244df4a9
Mindy Preston <meetup@yomimono.org>
2017-09-29 11:08:16 -0500

    start mon from bun

--- src/bun.ml
+++ src/bun.ml
@@ -3,41 +3,59 @@ let program =
   let doc = "Fuzz this program.  (Ideally it's a Crowbar test.)" in
   Cmdliner.Arg.(required & pos 0 obligatory_file None & info [] ~docv:"PROGRAM"
                   ~doc)
+let verbosity =
+  let doc = "Report on intermediate progress." in
+  Cmdliner.Arg.(value & flag_all & info ["v"] ~docv:"VERBOSE" ~doc)
+
+let fpath_conv = Cmdliner.Arg.conv Fpath.(of_string, pp)
 
 let input_dir =
   let doc = "Cache of inputs to use in fuzzing the program.  Will be passed \
   through to the program as the input parameter." in
-  Cmdliner.Arg.(value & opt dir "input" & info ["input"] ~docv:"INPUT" ~doc)
+  Cmdliner.Arg.(value & opt fpath_conv (Fpath.v "input")
+                & info ["input"] ~docv:"INPUT" ~doc)
 
 let output_dir =
   let doc = "Where to instruct the fuzzer to put its output." in
-  Cmdliner.Arg.(value & opt dir "output" & info ["output"] ~docv:"OUTPUT" ~doc)
+  Cmdliner.Arg.(value & opt fpath_conv (Fpath.v "output")
+                & info ["output"] ~docv:"OUTPUT" ~doc)
 
 let fuzzer =
   let doc = "The fuzzer to invoke." in
-  Cmdliner.Arg.(value & opt file "/usr/local/bin/afl-fuzz" & info ["fuzzer"] ~docv:"FUZZER" ~doc)
+  Cmdliner.Arg.(value & opt file "/usr/local/bin/afl-fuzz"
+                & info ["fuzzer"] ~docv:"FUZZER" ~doc)
 
-let fuzz fuzzer input output program : (unit, Rresult.R.msg) result =
+let fuzz verbosity fuzzer input output program : (unit, Rresult.R.msg) result =
   match Bos.OS.Cmd.exists (Bos.Cmd.(v fuzzer)) with
   | Ok true ->
     let null = Bos.OS.File.null in
     let null_fd = Unix.openfile (Fpath.to_string null) [] 0o000 in
     let pid = Spawn.spawn ~stdout:null_fd ~prog:fuzzer
-        ~argv:[fuzzer; "-i"; input; "-o"; output; "--"; program; "@@"] () in
+        ~argv:[fuzzer; "-i"; (Fpath.to_string input);
+               "-o"; (Fpath.to_string output);
+               "--"; program; "@@"] () in
     Unix.close null_fd;
-    Printf.printf "%s launched: PID %d\n%!" fuzzer pid;
-    Ok ()
+    if (List.length verbosity) >1 then Printf.printf "%s launched: PID %d\n%!" fuzzer pid;
+    (* monitor the process we just started with `mon`, and kill it when useful
+       results have been obtained *)
+    Common.mon verbosity false false None output
   | Ok false ->
     Error (`Msg (fuzzer ^ " not found - please ensure it exists and is an executable file"))
   | Error (`Msg e) ->
     Error (`Msg ("couldn't try to find " ^ fuzzer ^ ": " ^ e))
 
-let fuzz_t = Cmdliner.Term.(const fuzz $ fuzzer $ input_dir $ output_dir
+let fuzz_t = Cmdliner.Term.(const fuzz
+                            $ verbosity $ fuzzer (* bun/mon args *)
+                            $ input_dir $ output_dir (* fuzzer flags *)
                             $ program)
 
 let bun_info =
   let doc = "invoke afl-fuzz on a program in a CI-friendly way" in
-  Cmdliner.Term.(info ~version:"%%VERSION%%" ~doc "bun")
+  Cmdliner.Term.(info ~version:"%%VERSION%%" ~exits:default_exits ~doc "bun")
 
-let () = Cmdliner.Term.(exit @@ eval (fuzz_t, bun_info))
-
+let () = Cmdliner.Term.(exit @@ match eval (fuzz_t, bun_info) with
+    | `Ok (Ok ()) -> `Ok (Ok ())
+    | `Ok (Error (`Msg s)) -> Printf.eprintf "%s\n%!" s;
+      `Error `Exn (* not quite, but close enough I guess *)
+    | a -> a
+  )
---
+++ src/common.ml
@@ -0,0 +1,85 @@
+let get_stats lines =
+  (* did someone say shotgun parsers? *)
+  (* separate on : , keep only length 2 lists *)
+  let lines = List.map (Astring.String.fields ~empty:false ~is_sep:((=) ':'))
+      lines |> List.filter (fun a -> 2 = List.length a) in
+  (* drop whitespace *)
+  let lines = List.map (List.map Astring.String.trim) lines in
+  (* convert 2-length lists to tuples *)
+  List.map (function hd::tl::[] -> hd,tl | _ -> assert false) lines
+
+let try_kill pid =
+  try
+    (* cheap and dirty sanitization -- it would be a weird attack vector,
+       admittedly *)
+    Bos.OS.Cmd.run Bos.Cmd.(empty % "kill" % string_of_int (int_of_string pid))
+  with
+  | Invalid_argument _ -> Error (`Msg "fuzzer_pid is not a valid int; refusing \
+                                       to kill")
+let lookup s l = List.find_opt (fun (a,_) -> Astring.String.equal a s) l
+
+let print_stats verbose lines =
+  let default d = function
+    | None -> d
+    | Some (_, p) -> p
+  in
+  match List.length verbose with
+  | 0 -> ()
+  | _ ->
+    let execs = lookup "execs_per_sec" lines |> default "an unknowable number of" in
+    let paths = lookup "paths_found" lines |> default "an unknowable number of" in
+    Printf.printf "fuzzing hard at %s executions per second, having already \
+    discovered %s execution paths\n%!" execs paths
+
+let print_crashes output_dir =
+  let crashes = Fpath.(output_dir / "crashes" / "id$(file)" ) in
+  match Bos.OS.Path.matches crashes with
+  | Error (`Msg e) ->
+    Error (`Msg(Format.asprintf "Failure finding crashes in \
+                                 directory %a: %s" Fpath.pp crashes e))
+  | Ok crashes ->
+    try
+      List.iter (fun c -> Bos.OS.Cmd.run @@
+                          Bos.Cmd.(v "base64" % (Fpath.to_string c)) |>
+                          Rresult.R.get_ok) crashes;
+      Ok ()
+    with
+    | Invalid_argument e -> Error (`Msg (Format.asprintf "Failed to base64 a \
+    crash file: %s" e))
+
+let rec mon verbose humane oneshot stats output_dir : (unit, Rresult.R.msg) result =
+  let stats = match stats with
+    | None -> Fpath.(output_dir / "fuzzer_stats")
+    | Some stats -> stats
+  in
+  match Bos.OS.File.read_lines stats with
+  | Error (`Msg e) ->
+    Error (`Msg (Format.asprintf "Error reading stats file %a: %s" Fpath.pp stats e))
+  | Ok lines ->
+    let default d = function
+      | None -> d
+      | Some (_, p) -> try int_of_string p with Invalid_argument _ -> d
+    in
+    let lines = get_stats lines in
+    let crashes = lookup "unique_crashes" lines in
+    let cycles = lookup "cycles_done" lines in
+    match lookup "fuzzer_pid" lines with
+    | None -> Error (`Msg (Format.asprintf
+                             "no PID for the fuzzer found in stats file %a"
+                             Fpath.pp stats))
+    | Some (_, pid) ->
+      match (default 0 crashes, default 0 cycles) with
+      | 0, 0 ->
+        print_stats verbose lines;
+        if oneshot then Ok () else begin
+          Unix.sleep 60;
+          mon verbose humane oneshot (Some stats) output_dir
+        end
+      | 0, cycles ->
+        Printf.printf "%d cycles completed and no crashes found\n%!" cycles;
+        if humane then Ok () else try_kill pid
+      | crashes, _ ->
+        Printf.printf "%d crashes found! Take a look:\n%!" crashes;
+        let _ = print_crashes output_dir in
+        Printf.printf "Killing %s...\n%!" pid;
+        if humane then Ok () else try_kill pid
